8ddd284b93991260597d9d06ea24ebbb725fdc7a
Your Name <you@example.com>
2021-01-14 19:18:34 -0400

    init

---
+++ lib/pages/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Componentes Flutter'),
+          ),
+          body: _list()),
+    );
+  }
+
+  Widget _list() {
+    return ListView(
+      children: _listItens(),
+    );
+  }
+
+  List<Widget> _listItens() {
+    return [
+      ListTile(
+        title: Text('Test List'),
+      ),
+      Divider(),
+      ListTile(
+        title: Text('Test List'),
+      ),
+      Divider(),
+      ListTile(
+        title: Text('Test List'),
+      ),
+      Divider(),
+      ListTile(
+        title: Text('Test List'),
+      ),
+    ];
+  }
+}
