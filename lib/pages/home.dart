e4b837f5df90d1c3f532360b146fcb1f337a0254
manuel <devetechnologies@gmail.com>
2021-02-04 08:01:16 -0400

    update App Components

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:components_dev/providers/provider_menu.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,7 @@ class Home extends StatelessWidget {
   }
 
   Widget _list() {
+    print(menuProvider);
     return ListView(
       children: _listItens(),
     );
